36b6105018666790bd85d2370161b2d53078de81
Paul Swartz <paul@paulswartz.net>
2020-07-03 15:22:06 -0400

    test: run more property tests in CI

--- config/test.exs
+++ config/test.exs
@@ -5,3 +5,14 @@ config :logger,
   backends: []
 
 config :concentrate, :sink_s3, ex_aws: Concentrate.TestExAws
+
+# Configure StreamData to use more rounds in CI
+if System.get_env("CI") do
+  max_runs =
+    case System.get_env("STREAM_DATA_MAX_RUNS") do
+      nil -> 500
+      value -> String.to_integer(value)
+    end
+
+  config :stream_data, max_runs: max_runs
+end
