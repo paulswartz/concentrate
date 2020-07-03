c8eec412849ae3c776279816cf51c07184084ab0
Paul Swartz <paul@paulswartz.net>
2020-07-03 15:19:30 -0400

    feat: only use the trip + stop_sequence for merging StopTimeUpdate
    This avoids a race condition in the key, where it's calculated before the
    parent station data has been loaded from GTFS.

--- test/concentrate/stop_time_update_test.exs
+++ test/concentrate/stop_time_update_test.exs
@@ -28,15 +28,6 @@ defmodule Concentrate.StopTimeUpdateTest do
   end
 
   describe "Concentrate.Mergeable" do
-    test "key/1 uses the parent station ID" do
-      start_supervised!(Concentrate.Filter.GTFS.Stops)
-      Concentrate.Filter.GTFS.Stops._insert_mapping("child_id", "parent_id")
-
-      assert Mergeable.key(new(stop_id: "child_id")) == {nil, "parent_id", nil}
-      assert Mergeable.key(new(stop_id: "other")) == {nil, "other", nil}
-      assert Mergeable.key(new(stop_id: nil)) == {nil, nil, nil}
-    end
-
     test "merge/2 takes non-nil values, earliest arrival, latest departure" do
       first = @stu
 
